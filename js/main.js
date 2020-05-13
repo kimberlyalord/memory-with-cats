86c6e7e4d9719e41780b47dba32b3c6c053d2c9c
Kimberly Lord <kalord93@gmail.com>
2020-05-13 08:17:40 -0600

    Set up logic for computer's tile sequence

--- js/main.js
+++ js/main.js
@@ -28,16 +28,53 @@ initialize();
 function initialize() {
     playerSequence = [];
     tileSequence = [];
+    setTileSequence();
+}
+
+//test sequence
+tileSequence = [0, 1, 2, 3];
+
+function setTileSequence() {
+    let rand = Math.floor(Math.random() * 4);
+    tileSequence.push(rand);
+    console.log(tileSequence);
+    let idx = 0;
+    const playTileSequence = setInterval(function() {
+        if(tileSequence.length - 1 === idx) {
+            clearInterval(playTileSequence)
+        }
+        console.log(tileSequence[idx]);
+        if(idx === 0) {
+                document.getElementById('cat-one').style
+                .border = 'thick solid white';
+            }
+        if(idx === 1) {
+                document.getElementById('cat-two').style
+                .border = 'thick solid white';
+            }
+        if(idx === 2) {
+            document.getElementById('cat-three').style
+            .border = 'thick solid white';
+        }
+        if(idx === 3) {
+            document.getElementById('cat-four').style
+            .border = 'thick solid white';
+        }
+        //if statement for each tile to determine which gets a border, then
+        // figure out how to remove border from each(or ALL). 
+        idx++
+    }, 1000);
 }
 
 function handleClick(e) {
     // console.log('e.target', e.target.parentNode);
+    // tiles.forEach(tile => e.target.style.border = 'thick solid white'); 
     tiles.forEach(function(tile, index) {
         if (tile === e.target.parentNode)
             playerSequence.push(index);
     });
-    // tiles.style.border = 'white'; 
-    // console.log(playerSequence);
+    console.log(playerSequence);
+    checkForMatch();
     render();
 }
 
@@ -45,11 +82,8 @@ function render() {
     // console.log(tiles);
 }
 
-// test checkForMatch - currently displays wrong message? 
-tileSequence = [0, 2, 0];
-
 function checkForMatch() {
-    if(playerSequence === tileSequence) {
+    if(JSON.stringify(playerSequence) === JSON.stringify(tileSequence)) {
         messageEl.innerHTML = `Congratulations! Your current score is ${playerSequence.length}. Click Start to play the next sequence.`;
     } else {
         messageEl.innerHTML = `Sorry, you missed it. Your score was ${playerSequence.length - 1}.`;
